c74be1b536fed80e5f42fc9df369f35661d851c7
Raj Vardhan Singh <consults@gmail.com>
2011-08-16 04:52:08 +0100

    rails new folio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Folio::Application.config.secret_token = '0448290d8761ad61b559ff474c091c41475873bfdec000c3f8c8ae7d21805f41c966a0fd9c3e453deab78718e9b6d41c5817cd28d955dbb7fad99e7ae5f3af8e'
